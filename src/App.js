6a8871d2e4917126c85f250a04b155543867a732
QwertygidQ <Qwertygid@gmail.com>
2019-02-18 14:43:21 +0300

    Sample text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Typography component="h1" variant="h3">
+                Sample Text
+            </Typography>
+        );
+    }
 }
 
 export default App;
